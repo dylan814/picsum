592ff2cf5324550697595501764eec57ac36b817
Dylan Pereira <perdylan@gmail.com>
2020-05-29 20:22:50 +1000

    Updates

---
+++ precache-manifest.c3b30113d241637871bbb69e098344ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad62f0fcace7dbf135205f8bf503eecf",
+    "url": "/picsum/index.html"
+  },
+  {
+    "revision": "e8f0048bd4052fe874e9",
+    "url": "/picsum/static/css/main.88a3f9aa.chunk.css"
+  },
+  {
+    "revision": "318072f118930814af1b",
+    "url": "/picsum/static/js/2.e93f2129.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/picsum/static/js/2.e93f2129.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8f0048bd4052fe874e9",
+    "url": "/picsum/static/js/main.1e7a6deb.chunk.js"
+  },
+  {
+    "revision": "59f9017b8fbd426ecc08",
+    "url": "/picsum/static/js/runtime-main.5d8d98a8.js"
+  }
+]);
